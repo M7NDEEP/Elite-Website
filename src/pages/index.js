a20246fe3df4c5cc225842bc82b666e2553151e2
morningstar <mandeep7yadav@gmail.com>
2024-03-31 20:54:57 +0530

    2ndDone

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,13 +38,12 @@ export default function Home() {
   const teamMembers = [
     { name: "Mandeep Yadav", role: "Web Developer", imageSrc: "mandeep.png" ,insta:'https://www.instagram.com/themandeepyadav/',linkdin:'https://www.linkedin.com/in/mandeepyadav27',github:'https://github.com/M7NDEEP'},
     { name: "Mayank Matkar", role: "Java Developer", imageSrc: "mayank.jpeg" ,insta:'https://www.instagram.com/mayank_matkar',linkdin:'https://www.linkedin.com/in/mayank-matkar-042003k',github:'https://github.com/Codermayankkk'},
-    { name: "Abhigya Sharma", role: "Content/Java Developer", imageSrc: "abhigya.jpeg" ,insta:'https://www.instagram.com/itz_abhisss',linkdin:'https://www.linkedin.com/in/abhigya-sharma-88a929288',github:'https://github.com/abhigyaabs2'},
+    { name: "Abhigya Sharma", role: "Java Developer", imageSrc: "abhigya.jpeg" ,insta:'https://www.instagram.com/itz_abhisss',linkdin:'https://www.linkedin.com/in/abhigya-sharma-88a929288',github:'https://github.com/abhigyaabs2'},
     { name: "Aditya Patel", role: "Web Developer", imageSrc: "aditya.jpeg" ,insta:'https://www.instagram.com/i_aditya.p/',linkdin:'https://www.linkedin.com/in/adityapatel09',github:'https://github.com/iampateladitya'},
     { name: "Khushbu Gehlot", role: "Web Developer", imageSrc: "khushbu.jpeg" ,insta:'https://www.instagram.com/khushbugehlot___',linkdin:'https://www.linkedin.com/in/khushbu-gehlot-15a38b214',github:'https://github.com/khushbugehlot31'},
     { name: "Kripansh Kumrawat", role: "Java/Python Developer", imageSrc: "kripansh.jpg" ,insta:'https://www.instagram.com/_kripansh_',linkdin:'http://linkedin.com/in/kripansh29',github:'http://github.com/shallow-universe'},
     { name: "Nawadha Jadiya", role: "Java/Flutter Developer", imageSrc: "navadha.jpeg" ,insta:'https://www.instagram.com/nawadhaaa___',linkdin:'https://www.linkedin.com/in/nawadha-jadiya-aab426253',github:'https://github.com/NawadhaJadiya'},
     { name: "Ayush Maddhesiya", role: "Backend Developer", imageSrc: "ayush.jpeg" ,insta:'#',linkdin:'https://www.linkedin.com/in/ayush-maddhesiya/',github:'https://github.com/ayush-maddhesiya'},
-    { name: "Vaibhav Modi", role: "Frontend Developer", imageSrc: "vaibhav.jpeg" ,insta:'https://www.instagram.com/vaibhav_modi_45',linkdin:'https://www.linkedin.com/in/vaibhav-modi-59a775244/',github:'https://github.com/vaibhavmodi45'},
   ];
 
   const handleMouseEnter = (imgSrc) => {
